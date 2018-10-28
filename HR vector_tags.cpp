2db0968b5cde9512f647049cda73afb2a2d15201
Trevor <16tsmith55@gmail.com>
2018-10-28 16:42:22 -0600

    first commit

---
+++ HR vector_tags.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(){
+    int N, Q;
+    std::vector<std::string> list;
+    std::cin >> N >> Q;
+    list.resize(N + Q);
+    
+    for(int i = 0; i < N + Q; i++ ){
+        std::string input;
+        getline(std::cin, input);
+        list.emplace_back(input);
+    }
+    for(auto i : list){
+        std::cout << i << std::endl;
+    }
+    
+}   
